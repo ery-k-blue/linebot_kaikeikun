4bab04b4ee131e81576430091ce4d05d953cb2da
yudai ueno <yudai7676@yahoo.co.jp>
2022-03-04 04:18:42 +0900

    paymentinfoテーブル定義

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .user import User
 from .group import Group
 from .user_group import user_group
+from .payment_info import PaymentInfo
---
+++ models/payment_info.py
@@ -0,0 +1,24 @@
+from db import Base
+from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import relationship
+
+
+class PaymentInfo(Base):
+    """
+    Paymentテーブル
+
+    id       : 主キー
+    payment  : 支払金額
+    user     : 支払ったユーザー
+    group    : 支払ったグループ
+
+    """
+    __tablename__ = 'payment_info'
+    id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    payment = Column(INTEGER(unsigned=True))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    group_id = Column(Integer, ForeignKey("group.id"))
+
+    user = relationship("User", back_populates="payment_info")
+    group = relationship("Group", back_populates="payment_info")
