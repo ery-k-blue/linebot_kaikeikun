aeeefddd513dd19bcd47986ddd29991bf563c61f
yudai ueno <yudai7676@yahoo.co.jp>
2022-03-04 06:02:56 +0900

    payment_infoに精算済みフラグ追加

--- models/payment_info.py
+++ models/payment_info.py
@@ -1,7 +1,6 @@
 from db import Base
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import Column, ForeignKey, Integer, Boolean
 from sqlalchemy.dialects.mysql import INTEGER
-from sqlalchemy.orm import relationship
 
 
 class PaymentInfo(Base):
@@ -17,5 +16,6 @@ class PaymentInfo(Base):
     __tablename__ = 'payment_info'
     id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
     payment = Column(INTEGER(unsigned=True))
+    is_settled = Column(Boolean, default=False)
     user_id = Column(Integer, ForeignKey("user.id"))
     group_id = Column(Integer, ForeignKey("group.id"))
