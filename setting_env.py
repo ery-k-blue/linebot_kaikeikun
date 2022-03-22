dc280e7f6b38f49e18ff4c09e3917a316ad6e98e
yudai ueno <yudai7676@yahoo.co.jp>
2022-03-22 20:22:07 +0900

    add

--- setting_env.py
+++ setting_env.py
@@ -6,7 +6,7 @@ load_dotenv("setting.env")
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
-PORT = os.getenv("PORT")
+PORT = int(os.environ.get("PORT", 5000))
 
 RDB_PATH = os.getenv("RDB_PATH")
 DB_FILE_PATH = os.getenv("DB_FILE_PATH")
