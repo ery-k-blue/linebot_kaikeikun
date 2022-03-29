71873b563117c9ed0b4f5db19daed9f019b9b20b
yudai ueno <yudai7676@yahoo.co.jp>
2022-03-29 15:52:23 +0900

    add

--- db.py
+++ db.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import sessionmaker
 import setting_env
 
 Base = declarative_base()
-engine = create_engine(setting_env.RDB_PATH, echo=False)
+print("setting_env.SQLITE3_PATH:{}".format(setting_env.SQLITE3_PATH))
+engine = create_engine(setting_env.SQLITE3_PATH, echo=False)
+# engine = create_engine(setting_env.RDB_PATH, echo=False)
 
 Session = sessionmaker(bind=engine)
 session = Session()
--- setting_env.py
+++ setting_env.py
@@ -9,4 +9,5 @@ CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 PORT = int(os.environ.get("PORT", 5000))
 
 RDB_PATH = os.getenv("RDB_PATH")
+SQLITE3_PATH = os.getenv("SQLITE3")
 DB_FILE_PATH = os.getenv("DB_FILE_PATH")
