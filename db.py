4124263b9441014a470547b96bd6123421066d03
yudai ueno <yudai7676@yahoo.co.jp>
2022-03-30 18:25:36 +0900

    add

--- db.py
+++ db.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker
 import setting_env
 
 Base = declarative_base()
-print("setting_env.SQLITE3_PATH:{}".format(setting_env.SQLITE3_PATH))
 engine = create_engine(setting_env.SQLITE3_PATH, echo=False)
 # engine = create_engine(setting_env.RDB_PATH, echo=False)
 Session = sessionmaker(bind=engine, expire_on_commit=False)
